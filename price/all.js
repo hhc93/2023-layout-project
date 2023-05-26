b897bf2f2b36e9cc24ce0e1a1f2c4fe484117b93
hhc93 <clarahc30@gmail.com>
2023-05-27 00:24:08 +0800

    first commit -- arduino project create

---
+++ price/all.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+  $(".q1").click(function (event) {
+    // $(".img1").hide();
+    // $(".img11").show();
+    $(".hide1").toggle();
+  });
+  //   $(".q1").click(function (event) {
+  //     $(".img11").hide();
+  //     $(".hide1").show();
+  //     $(".img1").toggle();
+  //   });
+  $(".q2").click(function (event) {
+    $(".hide2").toggle();
+  });
+  $(".q3").click(function (event) {
+    $(".hide3").toggle();
+  });
+  $(".q4").click(function (event) {
+    $(".hide4").toggle();
+  });
+  $(".q5").click(function (event) {
+    $(".hide5").toggle();
+  });
+  //   $(".question").click(function (event) {
+  //     $("p").show();
+  //   });
+});
